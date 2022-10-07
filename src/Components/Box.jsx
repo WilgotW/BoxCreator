dc23b39d29f08d9a0fde3871310837827828a8a8
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-10-07 16:02:20 +0200

    -

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Box({width, height, rgb, objectType}) {
   return (
     <div key={Math.random() * 1000} style={{display: "flex", justifyContent: "center"}}>
-        <div style={{
-            width: width,
-            height: objectType == "Box" ? height : width  ,
-            background: rgb,
-            borderRadius: objectType == "Box" ? "0px" : "1000px"
-          }}>    
-        </div>
+      <div style={{
+          width: width,
+          height: objectType == "Box" ? height : width  ,
+          background: rgb,
+          borderRadius: objectType == "Box" ? "0px" : "1000px"
+        }}>    
+      </div>
     </div>
   )
 }
